9607c4b895752eb80f6d124210d3f98f1f5d11a5
Mathusha <mathuvk26@gmail.com>
2024-09-12 15:49:38 +0530

    ede

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BlogForm from './component/BlogForm';
+import BlogList from './component/BlogList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blog App</h1>
+      <BlogForm />
+      <BlogList />
     </div>
   );
-}
+};
 
 export default App;
